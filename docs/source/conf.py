a5fd79f5ebf17d86d059a1f51e52034e0f3ef958
platiumsecnet <platiumsecnet@gmail.com>
2020-02-07 13:39:23 +0000

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,7 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-#    "sphinx_rtd_theme",
-#    "groundwork",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -78,9 +77,9 @@ pygments_style = None
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 #html_theme = "bizstyle"
-html_theme = "groundwork"
+#html_theme = "groundwork"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
